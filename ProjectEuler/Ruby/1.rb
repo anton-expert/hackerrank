92d0a69eca620976c0f2c97862a5bad20ca2f307
anton benka <antonbenka666@gmail.com>
2015-12-01 16:17:27 +0100

    Project Euler #1 Ruby Code improved

--- ProjectEuler/Ruby/1.rb
+++ ProjectEuler/Ruby/1.rb
@@ -1,10 +1,11 @@
 t = gets.to_i
 
-def sum(n)
-	(1+n)*n/2
+def sum(n, b)
+	n = ((n-1)/b.to_f).floor
+	b*(1+n)*n/2
 end
 
 t.downto(1) do
-	n = gets.to_i - 1
-	puts 3*sum((n/3.0).floor) + 5*sum((n/5.0).floor) - 15*sum((n/15.0).floor)
+	n = gets.to_i
+	puts sum(n,3) + sum(n,5) - sum(n,15)
 end
