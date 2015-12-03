79dc0e3cafb45c88e32c8e5c0aa2dacb44281391
anton benka <antonbenka666@gmail.com>
2015-12-03 21:24:22 +0100

    Project Euler #1 C++ Code

--- ProjectEuler/C++/1.cpp
+++ ProjectEuler/C++/1.cpp
@@ -5,8 +5,18 @@
 #include <algorithm>
 using namespace std;
 
-
+long getSum(long n, int b) {
+    n = floor((n-1)/b);
+    return b*(1+n)*n/2;
+}
+    
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int count, num;
+    cin >> count;
+    
+    for(int i=0; i < count; i++) {
+        cin >> num;
+        cout<<getSum(num,3) + getSum(num,5)-getSum(num,15)<<endl;
+    }
     return 0;
 }
